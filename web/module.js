54c7812258b782c69627f65929903d8d5e6c4e7c
IMPP2001 <128334399+IMPP2001@users.noreply.github.com>
2023-03-20 09:22:48 +0700

    Add files via upload

---
+++ web/module.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const data = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>CPE499</title>
+        
+    </head>
+    <body>
+        <table>
+            <h1>ประวัติ นักศึกษา</h1>
+        
+            <div class = "a1">
+                <ul>
+                    
+                    <li>Name-Username : ปรเมธ หงษ์ทอง</li>
+                    <li>Student ID    : 6302454</li>
+                    <li>Gmail         : porameth.h63@rsu.ac.th</li>
+                    <li>Age           : 21 yo</li>
+                </ul>
+            </div>
+        </table>
+    </body>
+    </html>
+`
+
+module.exports = data;
